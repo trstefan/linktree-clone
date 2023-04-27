3fae553cac0273862f9d31f3535044beef25697c
xStef23 <trstefy@gmail.com>
2023-04-27 13:18:25 +0100

    code commited

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-gradient-to-r from-cyan-500 to-blue-500 h-max">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
